d68f81d96a30729c089e4dafd693d52c767e43e6
zeynep kadıoğlu <kadiogluzeynep@outlook.com>
2025-12-06 10:18:24 +0300

    ButtonLink

--- src/components/ButtonLink.tsx
+++ src/components/ButtonLink.tsx
@@ -1,8 +1,20 @@
-import { PrismicNextLink,  PrismicNextLinkProps } from "@prismicio/next";
+import { PrismicNextLink } from "@prismicio/next";
+import type { ComponentPropsWithoutRef } from "react";
 import clsx from "clsx";
 
-export default function ButtonLink({className, ...restProps}: PrismicNextLinkProps) {
+type ButtonLinkProps = ComponentPropsWithoutRef<typeof PrismicNextLink>;
+
+export default function ButtonLink({
+  className,
+  ...restProps
+}: ButtonLinkProps) {
   return (
-    <PrismicNextLink className={clsx("focus:ring-offset-3 relative inline-flex h-fit w-fit rounded-full border border-blue-100/20 bg-blue-200/10 px-4 py-2 text-blue-200 outline-none ring-yellow-300 transition-colors after:absolute after:inset-0 after:-z-10 after:animate-pulse after:rounded-full after:bg-yellow-100 after:bg-opacity-0 after:blur-md after:transition-all after:duration-500 hover:border-yellow-200/40 hover:text-yellow-300 after:hover:bg-opacity-15 focus:ring-2", className)} { ...restProps }/>
-  )
+    <PrismicNextLink
+      {...restProps}
+      className={clsx(
+        "focus:ring-offset-3 relative inline-flex h-fit w-fit rounded-full border border-blue-100/20 bg-blue-200/10 px-4 py-2 text-blue-200 outline-none ring-yellow-300 transition-colors after:absolute after:inset-0 after:-z-10 after:animate-pulse after:rounded-full after:bg-yellow-100 after:bg-opacity-0 after:blur-md after:transition-all after:duration-500 hover:border-yellow-200/40 hover:text-yellow-300 after:hover:bg-opacity-15 focus:ring-2",
+        className
+      )}
+    />
+  );
 }
